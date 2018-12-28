44f3ddc12a6e4392531f9e89fba879b38c7d57f6
FelSiq <felipe.siqueira@usp.br>
2018-12-28 15:25:21 -0200

    Quickfix + cormen's et al qsort

--- sortlib/sort.c
+++ sortlib/sort.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "sort.h"
 
+#define _PROGRAM_DRIVER 1
 #define HEAP_NODE_MASTER(NODE) (((NODE) - 1)/2)
 #define HEAP_NODE_SONL(NODE) (2*(NODE) + 1)
 #define HEAP_NODE_SONR(NODE) (2*(NODE) + 2)
@@ -46,14 +47,13 @@ static void _quicksort(
 	const size_t size_memb,
 	int (* const compare_func)(const void *, const void *)) {
 
-	register size_t tail_aux_ind = start_index, 
-		head_aux_ind = final_index;
-
 	if (start_index >= final_index)
 		return;
 
-	register const void *const pivot = vector + 
-		size_memb * ((final_index + start_index)/(2 * size_memb));
+	register long 	tail_aux_ind = start_index, 
+			head_aux_ind = final_index;
+
+	register const void *const pivot = vector + start_index;
 
 	while (tail_aux_ind <= head_aux_ind) {
 		while(compare_func(vector + tail_aux_ind, pivot) < 0)
@@ -226,3 +226,103 @@ void heapsort(
 #undef HEAP_NODE_MASTER
 #undef HEAP_NODE_SONL
 #undef HEAP_NODE_SONR
+
+static inline long __partition(
+	void *const vector,
+	const long ind_start,
+	const long ind_end,
+	const size_t size_memb,
+	int (*const compare_func)(const void *, const void *)) {
+
+	// Randomize pivot to avoid worst-case scenario
+	size_t const pivot_index = (size_t) ((rand()/ (1.0 * RAND_MAX)) 
+		* (ind_end - ind_start + 1) + ind_start);
+	__swap(vector + size_memb*pivot_index, 
+		vector + size_memb*ind_end, size_memb);
+	
+	register void const *pivot = vector + size_memb*ind_end;
+	long split_index = ind_start - 1;
+
+	for (register long j = ind_start; j < ind_end; j++) {
+		if (compare_func(vector + j*size_memb, pivot) <= 0) {
+			split_index++;
+			__swap(vector + size_memb*split_index, 
+				vector + size_memb*j, size_memb);
+		}
+	}
+	
+	split_index++;
+
+	__swap(vector + size_memb*split_index, 
+		vector + size_memb*ind_end, size_memb);
+
+	return split_index;
+}
+
+static void _quicksort2(
+	void *const vector,
+	const long ind_start,
+	const long ind_end,
+	const size_t size_memb,
+	int (*const compare_func)(const void *, const void *)) {
+
+	if (ind_start < ind_end) {
+		long const q = __partition(
+			vector,
+			ind_start,
+			ind_end,
+			size_memb,
+			compare_func
+		);
+
+		_quicksort2(vector, ind_start, q-1, size_memb, compare_func);
+		_quicksort2(vector, q+1, ind_end, size_memb, compare_func);
+	}
+}
+
+void quicksort2(
+	void *const vector,
+	const size_t num_memb,
+	const size_t size_memb,
+	int (*const compare_func)(const void *, const void *)) {
+
+	_quicksort2(vector, 0, num_memb-1, size_memb, compare_func);
+}
+	
+
+
+#if _PROGRAM_DRIVER
+#define _PD_VEC_SIZE 5516
+#define _PD_VEC_MAX 90324
+#define _PD_VEC_MIN -90234
+#define _PD_PRINT_MASK "%.2lf "
+typedef double _PD_VEC_TYPE;
+
+
+int main(int argc, char *const argv[]) {
+	_PD_VEC_TYPE *vec = malloc(sizeof(_PD_VEC_TYPE) * _PD_VEC_SIZE);
+	for (size_t i = 0; i < _PD_VEC_SIZE; i++) {
+		vec[i] = 1;
+		/*
+		vec[i] = (_PD_VEC_TYPE) ((rand() / (1.0 * RAND_MAX)) *
+			(_PD_VEC_MAX - _PD_VEC_MIN + 1) + _PD_VEC_MIN);
+		*/
+		//printf(_PD_PRINT_MASK, vec[i]); 
+	}
+	//puts("\n");
+
+	quicksort2(vec, _PD_VEC_SIZE, sizeof(_PD_VEC_TYPE), compare_double);
+
+
+	/*
+	for (size_t i = 0; i < _PD_VEC_SIZE; i++)
+		printf(_PD_PRINT_MASK, vec[i]);
+	puts("\n");
+	*/
+
+	free(vec);
+
+	return 0;
+}
+
+#endif
