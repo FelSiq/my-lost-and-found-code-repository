6f376eb180f7fc9ae5f7278b2dcb47d9aa116f59
FelSiq <felipe.siqueira@usp.br>
2019-09-08 21:35:40 +0300

    Fixed rejection sampling experiments

--- random-number-gen/rejection_sampling.py
+++ random-number-gen/rejection_sampling.py
@@ -1,6 +1,7 @@
 import typing as t
 
 import numpy as np
+import scipy.stats
 
 
 def horners(x: t.Union[int, float], coeffs: np.ndarray) -> t.Union[int, float]:
@@ -17,10 +18,11 @@ def horners(x: t.Union[int, float], coeffs: np.ndarray) -> t.Union[int, float]:
     return val
 
 
-def normal_pdf(x: t.Union[int, float], mean: float = 0.0, var=1.0) -> float:
+def normal_pdf(x: t.Union[int, float], mean: float = 0.0,
+               var: float = 1.0) -> float:
     """Probability density of normal distribution."""
-    return 1.0 / (2.0 * np.pi * var)**0.5 * np.exp(-(x - mean)**2.0 /
-                                                   (2.0 * var))
+    return 1.0 / np.sqrt(2.0 * np.pi * var) * np.exp(-np.square(x - mean) /
+                                                     (2.0 * var))
 
 
 def rejection_sampling(func_a: t.Callable[[t.Union[int, float]], float],
@@ -33,76 +35,70 @@ def rejection_sampling(func_a: t.Callable[[t.Union[int, float]], float],
     if random_seed is not None:
         np.random.seed(random_seed)
 
-    vals = []  # type: t.List[float]
+    vals = np.zeros(samples_num)
 
-    while len(vals) < samples_num:
+    cur_ind = 0
+    while cur_ind < samples_num:
         random_point = func_b_sampler()
 
-        new_val = np.random.random() * func_b_coeff * func_b(random_point)
+        p = np.random.uniform(0, 1)
 
-        if new_val < func_a(random_point):
-            vals.append(random_point)
+        if p * func_b_coeff * func_b(random_point) < func_a(random_point):
+            vals[cur_ind] = random_point
+            cur_ind += 1
 
-    return np.array(vals)
+    return vals
 
 
 def _experiment_01():
     """Rejection sampling experiment 01."""
 
-    def complex_fun(x: t.Union[int, float, np.ndarray]) -> t.Union[np.ndarray, float]:
-        if 2 <= x < 3:
-            return 1.75 - 0.5 * x
+    _pdf_gen_1 = scipy.stats.norm(5, 1)
+    _pdf_gen_2 = scipy.stats.norm(-2, 3)
+    _pdf_gen_3 = scipy.stats.expon()
 
-        if 1 <= x < 2 or 3 <= x < 3.55:
-            return 7 + 0.5 * x
+    def complex_fun(
+            x: t.Union[int, float, np.ndarray]) -> t.Union[np.ndarray, float]:
+        return (0.25 * _pdf_gen_1.pdf(x) + 0.35 * _pdf_gen_2.pdf(x) +
+                0.4 * _pdf_gen_3.pdf(x))
 
-        if -2 <= x < 0.5:
-            return horners(x, [0.5, -0.25, 1])
-
-        return 0
-
-    N = 20000
+    N = 5000
     INTERVAL = (-10, 10)
-    B_COEFF = 90
+    B_COEFF = 10
     SEED = 1234
 
-    p = np.array([5, 0, 3, 5, 1])
     vals = np.linspace(*INTERVAL, num=N)
 
+    generator = scipy.stats.uniform(INTERVAL[0], np.ptp(INTERVAL))
+
     res1 = np.array([complex_fun(x) for x in vals])
+    res2 = B_COEFF * generator.pdf(vals)
 
-    mean = 1.2
-    var = 5
-
-    res2 = np.array([normal_pdf(x, mean=mean, var=var) for x in vals])
-
-    norm_fact = max(res1.max(), B_COEFF * res2.max())
-
-    plt.plot(vals, res1 / norm_fact)
-    plt.plot(vals, B_COEFF * res2 / norm_fact)
-
-    aux = rejection_sampling(
+    samples = rejection_sampling(
         func_a=complex_fun,
-        func_b=lambda x: normal_pdf(x, mean=mean, var=var),
-        func_b_sampler=lambda: mean + np.sqrt(var) * np.random.randn(),
+        func_b=generator.pdf,
+        func_b_sampler=lambda: np.random.uniform(*INTERVAL),
         func_b_coeff=B_COEFF,
         samples_num=N,
         random_seed=SEED)
 
-    plt.hist(aux, bins=np.ptp(INTERVAL), density=True)
+    plt.plot(vals, res1)
+    plt.plot(vals, res2)
+    plt.hist(samples, bins=64, density=True)
     plt.show()
 
 
 def _experiment_02() -> None:
     """Rejection sampling experiment 02."""
 
-    def complex_fun(x: t.Union[int, float, np.ndarray]) -> t.Union[float, np.ndarray]:
-        return (0.30 * normal_pdf(x, mean=-2.0, var=1.2) + 0.60 * normal_pdf(
+    def complex_fun(
+            x: t.Union[int, float, np.ndarray]) -> t.Union[float, np.ndarray]:
+        return (0.40 * normal_pdf(x, mean=-2.0, var=1.2) + 0.70 * normal_pdf(
             x, mean=2.0, var=0.8) - 0.10 * normal_pdf(x, mean=-2.0, var=0.35))
 
     INTERVAL = (-10, 10)
-    N = 20000
-    B_COEFF = 2.5
+    N = 10000
+    B_COEFF = 3
     SEED = 1444
 
     mean = 0
@@ -128,12 +124,12 @@ def _experiment_02() -> None:
 
     plt.plot(vals, res1)
     plt.plot(vals, res2)
-    plt.hist(aux, bins=np.ptp(INTERVAL), density=True)
+    plt.hist(aux, bins=64, density=True)
     plt.show()
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    # _experiment_01()
+    _experiment_01()
     _experiment_02()
