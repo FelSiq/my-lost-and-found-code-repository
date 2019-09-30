0f673fdd3af279842f9a8dbb889f78fd78bdf13e
FelSiq <felipe.siqueira@usp.br>
2019-09-30 19:49:10 +0300

    Added query for the tf_idf code

--- tf_idf.py
+++ tf_idf.py
@@ -38,7 +38,7 @@ def filter_symbols(words: str, min_word_size: int = 3) -> t.Sequence[str]:
 
 
 def read_input(source_dir: str, file_extension: t.Optional[str] = None
-               ) -> t.Tuple[t.Sequence[str], t.Tuple[str, ...], np.ndarray]:
+               ) -> t.Tuple[np.ndarray, t.Tuple[str, ...], np.ndarray]:
     """Build a (Document x Word) frequency matrix from the documents within ``source_dir``."""
     filepaths = get_document_paths(
         source_dir=source_dir, file_extension=file_extension)
@@ -69,10 +69,15 @@ def read_input(source_dir: str, file_extension: t.Optional[str] = None
         for word, freq in zip(*document_freqs[filepath]):
             freq_mat[file_id, all_words_fixed.index(word)] += freq
 
-    return filepaths, all_words_fixed, freq_mat
+    return np.array(filepaths), all_words_fixed, freq_mat
 
 
-def calc_tf_idf_mat(freq_mat: np.ndarray) -> t.Tuple[np.ndarray, np.ndarray]:
+def calc_tf_idf_mat(freq_mat: np.ndarray,
+                    calc_vector_norm: bool = True,
+                    normalize_tf: bool = True,
+                    add_one_idf: bool = False
+                    ) -> t.Union[t.Tuple[np.ndarray, np.ndarray], t.
+                                 Tuple[np.ndarray, np.ndarray, np.ndarray]]:
     r"""Build the TD-IDF matrix from the (Document x Word) frequency matrix.
 
     Each entry of the TD-IDF matrix is calculated as follows:
@@ -83,9 +88,17 @@ def calc_tf_idf_mat(freq_mat: np.ndarray) -> t.Tuple[np.ndarray, np.ndarray]:
     $d_{j}$ is the number of documents that have at least one entry of the $j$th word.
     """
     num_docs, _ = freq_mat.shape
-    tf_idf_mat = freq_mat * np.log2(num_docs / (freq_mat > 0).sum(axis=0))
-    vector_len = np.linalg.norm(tf_idf_mat, ord=2, axis=1)
-    return tf_idf_mat, vector_len
+    idf_vec = add_one_idf + np.log2(num_docs / (freq_mat > 0).sum(axis=0))
+    tf_idf_mat = freq_mat * idf_vec
+
+    if normalize_tf:
+        tf_idf_mat /= freq_mat.sum(axis=1, keepdims=True)
+
+    if calc_vector_norm:
+        vector_norm = np.linalg.norm(tf_idf_mat, ord=2, axis=1)
+        return tf_idf_mat, idf_vec, vector_norm
+
+    return tf_idf_mat, idf_vec
 
 
 def cosine_similarity(vec_a: np.ndarray,
@@ -108,12 +121,76 @@ def cosine_similarity(vec_a: np.ndarray,
     return np.dot(vec_a, vec_b) / (vec_a_norm * vec_b_norm)
 
 
+def query(query_val: str,
+          tf_idf_mat: np.ndarray,
+          idf_vec: np.ndarray,
+          files: np.ndarray,
+          words: t.Tuple[str, ...],
+          top_k: int = 1,
+          doc_vec_lens: t.Optional[np.ndarray] = None) -> str:
+    """."""
+    if top_k <= 0:
+        raise ValueError("'top_k' must be a positive value!")
+
+    if not isinstance(top_k, int):
+        raise TypeError("'top_k' must be a integer value!")
+
+    tokens = filter_symbols(query_val)
+
+    top_k = min(top_k, files.size)
+
+    if top_k == files.size:
+        return files.copy()
+
+    vector_query = np.zeros(tf_idf_mat.shape[1])
+
+    for token, token_freq in zip(*np.unique(tokens, return_counts=True)):
+        try:
+            word_index = words.index(token)
+            vector_query[word_index] = token_freq * idf_vec[word_index]
+
+        except ValueError:
+            pass
+
+    vector_query /= len(tokens)
+
+    _vector_query_norm = np.linalg.norm(vector_query, ord=2)
+
+    if doc_vec_lens is None:
+        doc_vec_lens = np.linalg.norm(tf_idf_mat, ord=2, axis=1)
+
+    similarities = np.array([
+        cosine_similarity(
+            vector_doc,
+            vector_query,
+            vec_a_norm=doc_vec_len,
+            vec_b_norm=_vector_query_norm)
+        for doc_vec_len, vector_doc in zip(doc_vec_lens, tf_idf_mat)
+    ])
+
+    if top_k == 1:
+        return files[similarities.argmax()]
+
+    return files[np.argsort(similarities) < top_k]
+
+
 def _test():
     import sys
+    if len(sys.argv) < 2:
+        print("usage: python", sys.argv[0], "<source dir>")
+        exit(1)
+
     files, words, freq_mat = read_input(sys.argv[1])
-    print(files)
-    print(words)
-    print(calc_tf_idf_mat(freq_mat))
+    tf_idf_mat, idf_vec, doc_vec_lens = calc_tf_idf_mat(freq_mat)
+    print(
+        query(
+            "New new york",
+            tf_idf_mat=tf_idf_mat,
+            idf_vec=idf_vec,
+            files=files,
+            words=words,
+            top_k=2,
+            doc_vec_lens=doc_vec_lens))
 
 
 if __name__ == "__main__":
