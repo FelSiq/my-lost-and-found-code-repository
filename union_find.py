53c225e321ce5b7fcd6f07cf09f8319004bb9ec6
felsiq <felipe.siqueira@usp.br>
2020-10-31 23:52:53 -0300

    Added simpler code for union-find

---
+++ union_find.py
@@ -0,0 +1,17 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+
+        return x
+
+    def union(u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+
+        if pu != pv:
+            self.parent[pu] = pv
