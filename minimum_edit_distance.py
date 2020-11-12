024e87044e187fd136ca24a9ff1844809e3cc88f
felsiq <felipe.siqueira@usp.br>
2020-11-11 23:32:04 -0300

    Add MED for variable costs per operation

--- minimum_edit_distance.py
+++ minimum_edit_distance.py
@@ -56,6 +56,32 @@ def med(word_a: str, word_b: str) -> int:
     return memo[len(word_a) % 2][-1]
 
 
+def med_var_costs(
+    word_a: str, word_b: str, cost_ins: int = 1, cost_del: int = 1, cost_rep: int = 2
+) -> int:
+    memo = [(1 + len(word_b)) * [0] for _ in range(1 + len(word_a))]
+
+    for i in range(1, len(word_a) + 1):
+        memo[i][0] = cost_ins + memo[i - 1][0]
+
+    for j in range(1, len(word_b) + 1):
+        memo[0][j] = cost_del + memo[0][j - 1]
+
+    for i in range(1, len(word_a) + 1):
+        for j in range(1, len(word_b) + 1):
+            if word_a[i - 1] == word_b[j - 1]:
+                memo[i][j] = memo[i - 1][j - 1]
+
+            else:
+                memo[i][j] = min(
+                    cost_del + memo[i - 1][j],
+                    cost_ins + memo[i][j - 1],
+                    cost_rep + memo[i - 1][j - 1],
+                )
+
+    return memo[-1][-1]
+
+
 def _test():
     assert med("abc", "abcd") == 1
     assert med("abc", "dbc") == 2
@@ -68,9 +94,9 @@ def _test():
 
     import random
 
-    random.seed(128)
+    random.seed(32)
 
-    for i in range(500):
+    for i in range(50000):
         print(i, end="... ")
         size_a = random.randint(0, 40)
         size_b = random.randint(0, 40)
@@ -80,7 +106,9 @@ def _test():
         str_b = "".join(
             [chr(random.randint(ord("a"), 1 + ord("z"))) for _ in range(size_b)]
         )
-        assert med_rec(str_a, str_b) == med(str_a, str_b), f"{str_a}, {str_b}"
+        assert (
+            med_rec(str_a, str_b) == med(str_a, str_b) == med_var_costs(str_a, str_b)
+        ), f"{str_a}, {str_b}"
         print("ok")
 
 
