2ac93670a46ccba6855be99e4d2817efe6c6a89f
felsiq <felipe.siqueira@usp.br>
2020-11-15 21:14:11 -0300

    Add Union-Find with union by rank

--- union_find.py
+++ union_find.py
@@ -1,4 +1,6 @@
 class UnionFind:
+    """With path compression."""
+
     def __init__(self, n):
         self.parent = list(range(n))
 
@@ -8,7 +10,38 @@ class UnionFind:
 
         return self.parent[x]
 
-    def union(u, v):
+    def union(self, u, v):
         pu = self.find(u)
         pv = self.find(v)
         self.parent[pu] = pv
+
+
+class UnionFindRank:
+    """With path compression + union by rank."""
+
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = n * [0]
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+
+        return self.parent[x]
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+
+        if pu == pv:
+            return
+
+        if self.rank[pu] < self.rank[pv]:
+            self.parent[pu] = pv
+
+        elif self.rank[pu] > self.rank[pv]:
+            self.parent[pv] = pu
+
+        else:
+            self.parent[pu] = pv
+            self.rank[pv] += 1
