d2e02337c48e8af9f0172295198f44a364932a97
felsiq <felipe.siqueira@usp.br>
2021-05-11 22:03:29 -0300

    Add projection matrix calculation

--- geometry_and_alg_lin/vector_projection.py
+++ geometry_and_alg_lin/vector_projection.py
@@ -6,13 +6,13 @@ def magnitude_of_projection_of_u_onto_v(u, v):
     # fact 1: cos(u, v) = np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
     # fact 2: cos(u, v) = np.linalg.norm(proj) / np.linalg.norm(u)
     # Therefore: np.linalg.norm(proj) = np.dot(u, v) / np.linalg.norm(v)
-    return np.dot(u, v) / np.linalg.norm(v)
+    return np.dot(u, v) / float(np.linalg.norm(v))
 
 
 def direction_of_projection(v):
     # Since the projection of u onto v has the same direction of v,
     # the unity vector of the projection is the same unity vector of v.
-    return v / np.linalg.norm(v)
+    return v / float(np.linalg.norm(v))
 
 
 def slow_project_u_onto_v(u, v):
@@ -24,17 +24,27 @@ def project_u_onto_v(u, v, assume_v_unitary: bool = False):
     proj = np.dot(u, v) * v
 
     if not assume_v_unitary:
-        proj /= np.linalg.norm(v) ** 2
+        proj /= float(np.linalg.norm(v)) ** 2
 
     return proj
+
+
+def projection_mat(v, assume_v_unitary: bool = False):
+    proj_mat = np.outer(v, v)
+
+    if not assume_v_unitary:
+        proj_mat /= float(np.dot(v, v))
+
+    return proj_mat
 
 
 def _test():
     num_tests = 10000
     np.random.seed(32)
-    for i, (u, v) in enumerate(np.random.randn(num_tests, 2), 1):
+    for i, (u, v) in enumerate(np.random.randn(num_tests, 2, 17), 1):
         print(f"Test {i} ...")
-        assert np.isclose(project_u_onto_v(u, v), slow_project_u_onto_v(u, v))
+        assert np.allclose(project_u_onto_v(u, v), slow_project_u_onto_v(u, v))
+        assert np.allclose(projection_mat(v) @ u, project_u_onto_v(u, v))
 
 
 if __name__ == "__main__":
