cab5f2e85b9a1cd785c939122ec38c84482264fb
felsiq <felipe.siqueira@usp.br>
2021-05-12 00:35:33 -0300

    Now PLU decomposition more similar to scipy's

--- geometry_and_alg_lin/plu_decomposition.py
+++ geometry_and_alg_lin/plu_decomposition.py
@@ -3,21 +3,21 @@ import typing as t
 import numpy as np
 
 
-def _search_pivot_ind(U, r, c):
-    pivot_ind = r + np.argmax(np.abs(U[r:, c]))
+def _search_partial_pivot_ind(U, r, c):
+    partial_pivot_ind = r + np.argmax(np.abs(U[r:, c]))
 
-    if np.abs(U[pivot_ind, c]) < 1e-6:
+    if np.abs(U[partial_pivot_ind, c]) < 1e-7:
         # Note: just an invalid index
-        pivot_ind = U.shape[0]
+        partial_pivot_ind = U.shape[0] + 1
 
-    return pivot_ind
+    return partial_pivot_ind
 
 
 def _eliminate_column(U, r, c):
     n = U.shape[0]
     pivot = U[r, c]
 
-    coeffs = np.zeros(n - r - 1, dtype=float)
+    coeffs = np.empty(n - r - 1, dtype=float)
 
     for j in range(r + 1, n):
         coeff = U[j, c] / pivot
@@ -32,7 +32,7 @@ def _permute_rows(i, j, *args):
         return
 
     for arr in args:
-        arr[[i, j]] = arr[[j, i]]
+        arr[[i, j], :] = arr[[j, i], :]
 
 
 def _permute_cols(i, j, *args):
@@ -50,32 +50,29 @@ def plu(M):
     L is a lower triangular matrix; and
     U is a upper triangular matrix.
     """
-    M = np.asfarray(M)
-    n, m = M.shape
+    U = np.array(M, dtype=float)
+
+    n, m = U.shape
+    k = min(n, m)
 
     P = np.eye(n, dtype=np.uint8)
     L = np.eye(n, dtype=float)
-    U = np.copy(M)
 
     r, c = 0, 0
 
-    while r < n - 1 and c < m:
-        pivot_ind = _search_pivot_ind(U, r, c)
+    while r < min(k, n - 1) and c < k:
+        partial_pivot_ind = _search_partial_pivot_ind(U, r, c)
 
-        if pivot_ind >= n:
-            c += 1
-            continue
+        if partial_pivot_ind < n:
+            _permute_rows(partial_pivot_ind, r, U, L)
+            _permute_cols(partial_pivot_ind, r, L, P)
 
-        _permute_rows(pivot_ind, r, U, L)
-        _permute_cols(pivot_ind, r, L, P)
-
-        coeffs = _eliminate_column(U, r, c)
-        L[r + 1 :, r] = coeffs
+            coeffs = _eliminate_column(U, r, c)
+            L[r + 1 :, r] = coeffs
 
         r += 1
         c += 1
 
-    k = min(n, m)
     L = L[:, :k]
     U = U[:k, :]
 
@@ -85,41 +82,31 @@ def plu(M):
 def _test():
     import scipy.linalg
 
-    k = 1000
+    k = 20000
 
-    np.random.seed(32)
-    match = 0
+    np.random.seed(128)
+
+    not_match = 0
 
     for _ in np.arange(k):
-        r, c = np.random.randint(1, 20, size=2)
+        r, c = np.random.randint(1, 6, size=2)
         arr = np.random.randn(r, c)
         arr *= np.random.randint(2, size=arr.shape)
 
         P, L, U = plu(arr)
         sP, sL, sU = scipy.linalg.lu(arr, check_finite=False)
 
-        equal = (
+        if not (
             np.allclose(sP, P.astype(float))
             and np.allclose(sL, L)
             and np.allclose(sU, U)
-        )
-        if not equal:
-            print()
-            print(np.round(arr, 3))
-            print(np.round(P, 3))
-            print(np.round(sP, 3))
-            print(np.round(L, 3))
-            print(np.round(sL, 3))
-            print(np.round(U, 3))
-            print(np.round(sU, 3))
-            print()
-
-        match += int(equal)
+        ):
+            not_match += 1
 
         res = P @ L @ U
         assert np.allclose(res, arr)
 
-    print(match / k)
+    print("Not matches:", not_match / k)
 
 
 if __name__ == "__main__":
